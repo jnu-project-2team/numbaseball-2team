d7010075b6e0a0f9c93904ed2539cd9d5fe0e037
GhoRid <pgh5489@gmail.com>
2023-05-01 20:45:39 +0900

    feat: inspect duplicatoin
    Inspect duplication of input number

--- is_available.py
+++ is_available.py
@@ -6,4 +6,8 @@ def is_available(number, length):
     if len(number) != length:
         error_code = 1
 
+    # 중복검사
+    if len(number) != len(set(number)):
+        error_code = 2
+
     return error_code
